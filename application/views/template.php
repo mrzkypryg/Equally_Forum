abe9594685554dfa2171de09162d01ccf24e48a6
Muhammad Rizky Prayoga <mrpharyga2019@gmail.com>
2023-06-02 03:18:40 +0700

    Delete application directory

--- application/views/template.php
+++
@@ -1,8 +0,0 @@
-<?php  $uid = $this->session->userdata('uid'); ?>
-
-
-<?php $this->load->view('template/header'); ?>
-
-<?php $this->load->view($main_content); ?>
-
-<?php $this->load->view('template/footer'); ?>
