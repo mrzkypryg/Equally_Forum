668fc8e944f95339ff22d73d4e9f74927d159743
Muhammad Rizky Prayoga <mrpharyga2019@gmail.com>
2023-06-17 22:05:14 +0700

    Add kriteria dicoding challenge tag meta head

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Empowering Minds, Inspiring Action">
 	<meta name="author" content="Equally's Team">
+	<meta name="dicoding:email" content="mrpharyga2019@gmail.com">
 	<title>Equally</title>
 	<link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.png">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
